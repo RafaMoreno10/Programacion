6f1be683401973f4d0ae2777c8a57fbc53ec2252
Rafael Moreno <113892321+RafaMoreno10@users.noreply.github.com>
2022-11-24 10:18:20 +0100

    Add files via upload

---
+++ Tema2/Boletin_1_programacionModular/Ejercicio_10.py
@@ -0,0 +1,34 @@
+'''
+Created on Nov 24, 2022
+
+@author: estudiante
+'''
+def convertirBinario(cadena):
+    b=0
+    invertida=cadena[::-1]
+    for i in range(len(invertida)):
+        b+=(2 ** i) * (int(invertida[i]))
+    return b
+
+def convertirDecimal(numero):
+    result=""
+    divisor=2
+    dividendo=numero
+    while dividendo>=1:
+        resto=dividendo%divisor
+        dividendo=dividendo//divisor
+        result=str(resto)+result
+    return result
+
+numeroBin=input("Introduce numero binario: ")
+for i in range(len(numeroBin)):
+    while str(numeroBin[i]) != "0" and str(numeroBin[i]) != "1":
+        numeroBin=input("El numero introducido no es valido, introduce un numero binario: ")
+        
+print(f"El numero {numeroBin} en decimal es {convertirBinario(numeroBin)}")
+
+numDec=int(input("Introduzca un numero decimal:"))
+while numDec<0:
+    numDec=int(input("No valido, Introduzca un numero decimal:"))
+    
+print(f"El numero {numDec} en decimal es {convertirDecimal(numDec)}")
